cb06de3931337c30d15034a61707b7324519d436
mattkohl <hlaford@gmail.com>
2018-10-03 07:56:25 +0100

    more util functions

--- src/main/scala/com/mattkohl/Utils.scala
+++ src/main/scala/com/mattkohl/Utils.scala
@@ -14,10 +14,39 @@ object Utils {
   }
 
   def labelToVariable(label: String): String = label.replace(" ", "-").replace(".", " ")
+
   def labelToSparqlVariable(label: String): String = s"?${labelToVariable(label)}"
 
+  def addRdfsLabel(variable: String, labelValue: String): String = s"""$variable rdfs:label "$labelValue""""
 
-  def addRdfsLabel(variable: String, labelValue: String): String = { variable + " rdfs:label \"" + labelValue + "\" . " }
-  def addOptionalRdfsLabel(variable: String, labelVariable: String): String = { "OPTIONAL { " + variable + " rdfs:label " + labelVariable + " } . " }
+  def addOptionalRdfsLabel(variable: String, labelVariable: String): String = s"OPTIONAL { $variable rdfs:label $labelVariable } ."
 
+  def extractSparql(triples: List[Triple]): List[String] = triples flatMap { triple =>
+      val subjectVariable = labelToSparqlVariable(triple.state.s.label)
+      val subjectLabelVariable = s"${subjectVariable}Label"
+      if (triple.state.o.literal.nonEmpty) {
+        val objectVariable = labelToSparqlVariable(triple.state.o.literal)
+
+        List(
+          s"""$subjectVariable rdf:type <${triple.state.s.uri}> . """,
+          s"""$subjectVariable <${triple.state.p.uri}> $objectVariable . """,
+          addRdfsLabel(objectVariable, triple.state.o.literal),
+          addOptionalRdfsLabel(subjectVariable, subjectLabelVariable)
+        )
+
+      } else {
+        val objectVariable = labelToSparqlVariable(triple.state.o.label)
+        val objectLabelVariable = objectVariable + "Label"
+
+        List(
+          s"""$subjectVariable rdf:type <${triple.state.s.uri}> . """,
+          s"""$objectVariable rdf:type <${triple.state.o.uri}> . """,
+          s"""$subjectVariable <${triple.state.p.uri}> $objectVariable . """,
+          addOptionalRdfsLabel(subjectVariable, subjectLabelVariable),
+          addOptionalRdfsLabel(objectVariable, objectLabelVariable)
+        )
+
+      }
+  }  
+  
 }
